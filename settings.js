ce00c9ce20451712bc118b2ca264103073ffadcc
Lilpresh-hub <160262120+Lilpresh-hub@users.noreply.github.com>
2024-06-02 00:22:39 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@
 //YouTube: @DGXeon
 //Instagram: unicorn_xeon13
 //Telegram: t.me/xeonbotinc
-//GitHub: @DGXeon
-//WhatsApp: +916909137213
+//GitHub: @Lilpresh-hub
+//WhatsApp: +2349152597171
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
 
 const fs = require('fs')
@@ -15,23 +15,23 @@ global.sessionid ='ur session id'
 
 //owmner v card
 global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.socialm = "GitHub: Lilpresh-hub" //ur github or insta name
+global.location = "Africa, Nigeria, Lagos" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.botname = 'Presh Bot MD V11' //ur bot name
+global.ownernumber = '2349152597171' //ur owner number
+global.ownername = '🦄드림 가이 LONER' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
+global.themeemoji = '🦅'
+global.wm = "Presh Bot Inc."
+global.botscript = 'https://github.com/Lilpresh-hub/CheemsBot-MD11' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
+global.author = "🦄드림 가이 Presh\n\n+2349152597171"
 global.creator = "916909137213@s.whatsapp.net"
-global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.xprefix = '.','>'
+global.premium = ["2349152597171"] // Premium User
 global.hituet = 0
 
 //bot sett
@@ -41,16 +41,16 @@ global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
 global.anticall = false //bot blocks user when called
-global.autoswview = false //auto status/story view
+global.autoswview = true //auto status/story view
 global.adminevent = false //show promote/demote message
 global.groupevent = false //show update messages in group chat
 //msg
 global.mess = {
-	limit: 'Your limit is up!',
+	limit: 'Mumu your limit is up🙃😃!',
 	nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
-    done: 'Done✓',
-    error: 'Error!',
-    success: 'Here you go!'
+    done: 'E don set boss✓',
+    error: 'Omoh fvck!',
+    success: '👏wow!'
 }
 //thumbnail
 global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
